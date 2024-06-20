c5aba1d3b3bc5224add319b39cf32d18885fd0ed
Matti Laamanen <mattilaa@gmail.com>
2024-06-20 12:55:14 +0300

    Rewrite lua config and cleanup

--- lua/clangd.lua
+++ lua/clangd.lua
@@ -1,38 +1,33 @@
 -- Cpp stuff
-
-local util = require('lspconfig/util')
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
-})
-
-lsp.configure("clangd", {
-   cmd = {
-        "clangd",
-        "--clang-tidy",
-        "--background-index",
-        "--all-scopes-completion",
-        "--header-insertion=never",
-        "--completion-style=detailed",
-        "--cross-file-rename",
-   },
-   filetypes = { "c", "cpp", "objc", "objcpp" },
-   root_dir = util.root_pattern("compile_commands.json", ".git"),
-   settings = {
-       clangd = {
-           semanticHighlighting = true,
-           completion = {
-               filterAndSort = true,
-           },
-           diagnostics = {
-               disabled = { "clang-diagnostic-unused-parameter" },
-           },
-       },
-   },
-})
-
-lsp.setup()
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup {
+  cmd = {
+       "clangd",
+       "--clang-tidy",
+       "--background-index",
+       "--all-scopes-completion",
+       "--header-insertion=never",
+       "--completion-style=detailed",
+       "--cross-file-rename",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  root_dir = function(fname)
+    return lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname) or lspconfig.util.path.dirname(fname)
+  end,
+  settings = {
+    clangd = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      clangdFileStatus = true,
+      semanticHighlighting = true,
+      completion = {
+	      filterAndSort = true,
+      },
+      diagnostics = {
+	      disabled = { "clang-diagnostic-unused-parameter" },
+      },
+    }
+  }
+}
 
 
